468826a065634435349711095215a2a5506881b2
jjjghu <chiuliyou@gmail.com>
2026-01-25 12:18:51 +0800

    新增 tailwindcss

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,8 +3,11 @@ import remarkMath from 'remark-math';
 import rehypeKatex from 'rehype-katex';
 import remarkLeetCodeLink from './src/plugins/remark-leetcode-link.mjs';
 
+import tailwindcss from '@tailwindcss/vite';
+
 export default defineConfig({
   site: 'https://jjjghu.github.io',
+
   markdown: {
     remarkPlugins: [remarkMath, remarkLeetCodeLink],
     rehypePlugins: [rehypeKatex],
@@ -14,5 +17,9 @@ export default defineConfig({
       // 如果想要讓程式碼區塊自動換行，可以啟用 wrap
       wrap: true,
     },
+  },
+
+  vite: {
+    plugins: [tailwindcss()],
   },
 });
