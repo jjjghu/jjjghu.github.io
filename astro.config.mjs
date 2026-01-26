7188e9fab43e599f2a61fffaa75c915722d35358
jjjghu <chiuliyou@gmail.com>
2026-01-27 01:29:56 +0800

    新增文章, 加入 scrollbar 外觀

--- astro.config.mjs
+++ astro.config.mjs
@@ -9,10 +9,8 @@ export default defineConfig({
     remarkPlugins: [remarkMath, remarkLeetCodeLink],
     rehypePlugins: [rehypeKatex],
     shikiConfig: {
-      // 這裡選擇主題，例如: 'dracula', 'github-dark', 'monokai', 'one-dark-pro'
       theme: 'dark-plus',
-      // 如果想要讓程式碼區塊自動換行，可以啟用 wrap
-      wrap: true,
+      wrap: false,
     },
   },
 });
