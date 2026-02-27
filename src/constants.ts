5509e703bde7a6129314a1ca8881a3a4871c3085
jjjghu <chiuliyou@gmail.com>
2026-02-27 19:41:53 +0800

    網站腳本

--- src/constants.ts
+++ src/constants.ts
@@ -1,3 +1,4 @@
 export const LEETCODE_COLLECTION = 'leetcode' as const;
 export const ZEROJUDGE_COLLECTION = 'zerojudge' as const;
 export const ROADMAP_COLLECTION = 'roadmap' as const;
+export const GUIDE_COLLECTION = 'guide' as const;
